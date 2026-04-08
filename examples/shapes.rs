22cf182bab64c8515718841a459140ca294eee9f
Paulo <49159843+PauloUbirajara@users.noreply.github.com>
2026-04-07 22:54:43 -0300

    refactor: update project dimensions and reposition shape coordinates in shapes example

--- examples/shapes.rs
+++ examples/shapes.rs
@@ -1,19 +1,33 @@
-use motion_canvas_rs::engine::project::Project;
-use motion_canvas_rs::engine::nodes::{Circle, Rect, Line};
-use motion_canvas_rs::render::Color;
 use glam::Vec2;
+use motion_canvas_rs::engine::nodes::{Circle, Line, Rect};
+use motion_canvas_rs::engine::project::Project;
+use motion_canvas_rs::render::Color;
 
 fn main() -> anyhow::Result<()> {
-    let mut project = Project::new(800, 600);
+    let mut project = Project::new(400, 200);
 
-    let circle = Circle::new(Vec2::new(400.0, 300.0), 50.0, Color::rgb8(0xe1, 0x32, 0x38)); // Red
-    let rect = Rect::new(Vec2::new(100.0, 100.0), Vec2::new(200.0, 100.0), Color::rgb8(0x68, 0xab, 0xdf)) // Blue
-        .with_radius(10.0);
-    let line = Line::new(Vec2::new(0.0, 0.0), Vec2::new(100.0, 100.0), Color::rgb8(0xf2, 0xf2, 0xf2), 2.0); // White
+    // Red
+    let circle = Circle::new(Vec2::new(100.0, 100.0), 50.0, Color::rgb8(0xe1, 0x32, 0x38));
+
+    // Blue
+    let rect = Rect::new(
+        Vec2::new(150.0, 50.0),
+        Vec2::new(100.0, 100.0),
+        Color::rgb8(0x68, 0xab, 0xdf),
+    )
+    .with_radius(10.0);
+
+    // White
+    let line = Line::new(
+        Vec2::new(250.0, 100.0),
+        Vec2::new(350.0, 100.0),
+        Color::rgb8(0xf2, 0xf2, 0xf2),
+        2.0,
+    );
 
     project.scene.add(Box::new(circle));
     project.scene.add(Box::new(rect));
     project.scene.add(Box::new(line));
-    
+
     project.show()
 }
