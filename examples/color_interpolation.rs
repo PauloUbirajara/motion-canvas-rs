a2c454ce12e3cb7beeca14937f8772c035f660d0
Paulo <49159843+PauloUbirajara@users.noreply.github.com>
2026-05-18 21:32:07 -0300

    remove deprecations - add before assets

--- examples/color_interpolation.rs
+++ examples/color_interpolation.rs
@@ -21,10 +21,18 @@ fn main() {
     project.scene.video_timeline.add(loop_anim(
         move || {
             chain![
-                circle.fill_color.to(Color::YELLOW, duration),
-                circle.fill_color.to(Color::GREEN, duration),
-                circle.fill_color.to(Color::BLUE, duration),
-                circle.fill_color.to(Color::RED, duration),
+                circle
+                    .fill_paint
+                    .to(Some(Paint::Solid(Color::YELLOW)), duration),
+                circle
+                    .fill_paint
+                    .to(Some(Paint::Solid(Color::GREEN)), duration),
+                circle
+                    .fill_paint
+                    .to(Some(Paint::Solid(Color::BLUE)), duration),
+                circle
+                    .fill_paint
+                    .to(Some(Paint::Solid(Color::RED)), duration),
             ]
         },
         None,
--- examples/grid.rs
+++ examples/grid.rs
@@ -44,7 +44,8 @@ fn main() {
         all![
             grid.rows.to(16.0, Duration::from_secs(2)),
             grid.columns.to(16.0, Duration::from_secs(2)),
-            grid.stroke_color.to(Palette::BLUE, Duration::from_secs(2)),
+            grid.stroke_paint
+                .to(Some(Paint::Solid(Palette::BLUE)), Duration::from_secs(2)),
             grid.spacing
                 .to(Vec2::new(100.0, 100.0), Duration::from_secs(2)),
         ],
@@ -52,8 +53,8 @@ fn main() {
         all![
             grid.rows.to(8.0, Duration::from_secs(2)),
             grid.columns.to(8.0, Duration::from_secs(2)),
-            grid.stroke_color
-                .to(Palette::ORANGE, Duration::from_secs(2)),
+            grid.stroke_paint
+                .to(Some(Paint::Solid(Palette::ORANGE)), Duration::from_secs(2)),
             grid.spacing
                 .to(Vec2::new(20.0, 20.0), Duration::from_secs(2)),
         ],
